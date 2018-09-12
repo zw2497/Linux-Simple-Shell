07c9b7748eb38f5b1d59e1cc3aab234cea95105b
Zhicheng Wu <zhicheng.wu@columbia.edu>
2018-09-12 13:51:25 -0400

    add Buildin command cd

--- shell.c
+++ shell.c
@@ -1,3 +1,13 @@
+/*
+ * SPDX-License-Identifier: 0BSD
+ * Shell.c
+ * Shell
+ *
+ * Created by Zhicheng WU on 9/10/2018
+ * Copyright 2018 Zhicheng Wu.
+ * All rights reserved
+ */
+
 #include <stdio.h>
 #include "string.h"
 #include <errno.h>
@@ -5,75 +15,99 @@
 #include <unistd.h>
 #include <wait.h>
 
-void inputStdin (char **input, size_t *n) {
-    if (getline(input , n, stdin) == -1) {
-        fprintf(stderr, "error: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
+char* concat(const char *s1, const char *s2)
+{
+	char *result = malloc(strlen(s1) + strlen(s2) + 1);
+	strcpy(result, s1);
+	strcat(result, s2);
+	return result;
 }
 
-void tokenize(char *input, char **argument, int size) {
-    const char *delimiter = " \n";
-    int i = 0;
-    argument[0] = strtok(input, delimiter);
-
-    while (argument[i] != NULL) {
-        i++;
-        argument[i] = strtok(NULL, delimiter);
-        if (i > size - 2) {
-            size = size * 2;
-            argument = realloc(argument, size * sizeof(char*));
-        }
-    }
-
-
+void inputStdin(char **input, size_t *n)
+{
+	if (getline(input, n, stdin) == -1) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 }
 
-void  exeBuildin(char **args) {
-    int pid;
-    int status;
+void tokenize(char *input, char **argument, int size)
+{
+	const char *delimiter = "\n ";
+	int i = 0;
 
-    pid = fork ();
-    if (pid == 0)
-    {
-        if (execv(args[0], args) == -1) {
-            fprintf(stderr, "error: %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
+	argument[0] = strtok(input, delimiter);
+	while (argument[i] != NULL) {
+		i++;
+		argument[i] = strtok(NULL, delimiter);
+		if (i > size - 2) {
+			size = size * 2;
+			argument = realloc(argument, size * sizeof(char *));
+		}
+	}
 
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        /* The fork failed.  Report failure.  */
-        fprintf(stderr, "error: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    } else if (wait(&status) != pid) {
-        status = -1;
-    }
 
 }
 
+void exeBuildin(char **args)
+{
+	int pid;
+	int status;
 
-int main(void) {
-    extern int errno;
-    char **originStr;
-    size_t size = 100;
 
-    char **args;
-    int argNumber = 10;
-    originStr = malloc(argNumber * sizeof(char*));
-    args = malloc(argNumber * sizeof(char*));
+	pid = fork();
+	switch (pid) {
+	case 0:
+		if (execv(args[0], args) == -1) {
+			fprintf(stderr, "error: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+		break;
+	case -1:
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+		break;
+	default:
+		wait(&status);
+	}
 
-    int i = 0;
 
-    while (1) {
-        printf("$");
-        inputStdin(originStr, &size);
-        tokenize(*originStr, args, argNumber);
-        exeBuildin(args);
+}
 
-        if (i > 5) {
-            return 0;
-        }
-        i++;
-    }
+void buildin(char **args, char **origin)
+{
+	if (strcmp(args[0], "exit") == 0) {
+		free(args);
+		free(origin);
+		exit(EXIT_SUCCESS);
+	} else if (strcmp(args[0], "cd") == 0) {
+		if (chdir(args[1]) == -1) {
+			fprintf(stderr, "error: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		exeBuildin(args);
+	}
+
+}
+
+
+int main(void)
+{
+	char **originStr;
+	char **args;
+	int argNumber = 10;
+
+	originStr = malloc(argNumber * sizeof(char *));
+	args = malloc(argNumber * sizeof(char *));
+	size_t size = 100;
+
+	while (1) {
+		printf("$");
+		inputStdin(originStr, &size);
+		tokenize(*originStr, args, argNumber);
+		buildin(args, originStr);
+
+	}
 }
